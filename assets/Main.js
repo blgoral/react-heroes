86771da6cf62310096f96618a48bb640b6f82850
Blake Goral <blakegoral@gmail.com>
2021-03-22 23:02:52 -0500

    add Form to add hero

--- assets/Main.js
+++ assets/Main.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import HeroForm from './components/HeroForm';
 
-const Main = props => {
-    return <h1>Test</h1>;
+const Main = () => {
+    const [error, setError] = useState(null);
+    const [heroes, setHeroes] = useState([]);
+
+    const addHero = (hero) => {
+        console.log(JSON.stringify(hero));
+        fetch(
+            apiURL,
+            {
+                method: "POST",
+                body: JSON.stringify(hero),
+                headers: { "Content-Type": "application/json" },
+            }
+        )
+            .then(response => response.json())
+            .then(data => console.log(data));
+    };
+
+    return(
+      <HeroForm onAddHero={addHero} />
+    );
 }
 
-fetch('https://localhost:8000/api/heroes')
+const apiURL = 'https://localhost:8000/api/heroes';
+
+fetch(apiURL)
     .then(response => response.json())
     .then(data => console.log(data));
 
---
+++ assets/components/HeroForm.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+
+const HeroForm = props => {
+    const [heroName, setHeroName] = useState('');
+    const [heroLevel, setHeroLevel] = useState('');
+
+    const formHandler = event => {
+        event.preventDefault();
+        props.onAddHero({name: heroName, level: heroLevel});
+    }
+
+    return(
+        <section className="hero-form">
+            <form onSubmit={formHandler}>
+            <div className="form-control">
+                <label htmlFor="hero-name">Hero Name</label>
+                <input
+                    type="text"
+                    id="hero-name"
+                    value={heroName}
+                    onChange={event => {
+                        setHeroName(event.target.value);
+                    }}
+                />
+            </div>
+            <div className="form-control">
+                <label htmlFor="hero-level">Hero Level</label>
+                <input
+                    type="number"
+                    id="hero-level"
+                    value={heroLevel}
+                    onChange={event => {
+                        setHeroLevel(+event.target.value);
+                    }}
+                />
+            </div>
+                <button type="submit">Add Hero</button>
+            </form>
+        </section>
+    );
+};
+
+export default HeroForm;
