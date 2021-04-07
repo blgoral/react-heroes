9aed9f3b4554053cf37b437954c6deb4a0493d19
Blake Goral <blakegoral@gmail.com>
2021-04-06 21:27:43 -0500

    update data fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,17 +5,30 @@ namespace App\DataFixtures;
 use App\Entity\Hero;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Generator
+     */
+    protected $faker;
+
+
     public function load(ObjectManager $manager)
     {
-        $hero = new Hero();
+        $this->faker = Factory::create();
 
-        $hero->setName('Test Hero')
-            ->setLevel(rand(1, 100));
+        for($i = 0; $i <= 10; $i++) {
+            $hero = new Hero();
 
-        $manager->persist($hero);
+            $hero->setName($this->faker->firstName)
+                ->setLevel(rand(1, 100));
+
+            $manager->persist($hero);
+        }
+
 
         $manager->flush();
     }
